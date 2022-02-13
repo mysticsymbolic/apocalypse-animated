2df7adb60e091313c7cf6adad098e75882327f24
Atul Varma <varmaa@gmail.com>
2022-02-13 17:33:40 -0500

    Initial Commit

---
+++ apocalypse-animated/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  apocalypse-animated
+//
+//  Created by Atul Varma on 2/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ apocalypse-animated/apocalypse_animatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  apocalypse_animatedApp.swift
+//  apocalypse-animated
+//
+//  Created by Atul Varma on 2/13/22.
+//
+
+import SwiftUI
+
+@main
+struct apocalypse_animatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
