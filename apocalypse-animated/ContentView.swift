d4520cc0124c3807af9312a0d5f80fe7a1090e21
Atul Varma <varmaa@gmail.com>
2022-02-19 14:55:15 -0500

    Add a ChapterItem enum.

--- apocalypse-animated/ContentView.swift
+++ apocalypse-animated/ContentView.swift
@@ -7,17 +7,61 @@
 
 import SwiftUI
 
-struct DetailView: View {
+enum ChapterItem: Decodable, Hashable {
+    case Verse(text: String)
+    case Animation(basename: String, width: Int, height: Int)
+    
+    enum CodingKeys: String, CodingKey {
+        case type, text, basename, width, height
+    }
+    
+    enum ItemType: String, Decodable {
+        case verse, animation
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let type = try container.decode(ItemType.self, forKey: .type)
+        
+        switch type {
+        case .verse:
+            let text = try container.decode(String.self, forKey: .text)
+            self = .Verse(text: text)
+        case .animation:
+            let basename = try container.decode(String.self, forKey: .basename)
+            let width = try container.decode(Int.self, forKey: .width)
+            let height = try container.decode(Int.self, forKey: .height)
+            self = .Animation(basename: basename, width: width, height: height)
+        }
+    }
+}
+
+struct Chapter: Decodable, Hashable {
+    let title: String
+    let items: [ChapterItem]
+}
+
+let SampleChapter = Chapter(title: "Sample Chapter", items: [
+    ChapterItem.Verse(text: "Here is some text! Yup."),
+    ChapterItem.Verse(text: "Here is some more text!"),
+    ChapterItem.Animation(basename: "throne2_5", width: 450, height: 300),
+    ChapterItem.Verse(text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie diam dapibus ullamcorper fringilla. Nunc nulla purus, consequat non rutrum sed, rhoncus eu nunc. Curabitur eleifend leo vitae convallis bibendum. Curabitur et ligula nec purus ornare lacinia nec convallis dolor. Vivamus augue nibh, molestie id viverra in, fermentum vel turpis. Nam a neque lacus. Suspendisse eu tortor est. Ut eget tellus pellentesque, dapibus leo ut, dapibus elit. Aliquam interdum molestie lorem. Nullam semper imperdiet orci.")
+])
+
+struct ChapterView: View {
+    let data: Chapter
+    
     var body: some View {
         ScrollView {
             VStack {
-                Text("Here is some text!")
-                    .padding()
-                Text("Here is some more text!")
-                    .padding()
-                LoopingVideo(video: "throne2_5").frame(height: 300.0)
-                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie diam dapibus ullamcorper fringilla. Nunc nulla purus, consequat non rutrum sed, rhoncus eu nunc. Curabitur eleifend leo vitae convallis bibendum. Curabitur et ligula nec purus ornare lacinia nec convallis dolor. Vivamus augue nibh, molestie id viverra in, fermentum vel turpis. Nam a neque lacus. Suspendisse eu tortor est. Ut eget tellus pellentesque, dapibus leo ut, dapibus elit. Aliquam interdum molestie lorem. Nullam semper imperdiet orci.")
-                    .padding()
+                ForEach(self.data.items, id: \.self) { item in
+                    switch item {
+                    case .Verse(let text):
+                        Text(text).padding()
+                    case .Animation(let basename, _, let height):
+                        LoopingVideo(video: basename).frame(height: CGFloat(height))
+                    }
+                }
             }
         }
     }
@@ -27,7 +71,7 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             VStack {
-                NavigationLink(destination: DetailView()) {
+                NavigationLink(destination: ChapterView(data: SampleChapter)) {
                     Text("First view")
                 }
                 NavigationLink(destination: Text("here is another view")) {
